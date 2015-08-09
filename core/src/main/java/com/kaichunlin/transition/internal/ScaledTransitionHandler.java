b2c5afa0432e16fc2b29000ef0b3989db0c57d52
Kai-Chun Lin <kaichun.lin@gmail.com>
2015-08-09 11:56:33 +0800

    ScaledTransitionHandler.onUpdateScaledProgress return progress in the range [0..1]

--- core/src/main/java/com/kaichunlin/transition/internal/ScaledTransitionHandler.java
+++ core/src/main/java/com/kaichunlin/transition/internal/ScaledTransitionHandler.java
@@ -12,16 +12,37 @@ import com.kaichunlin.transition.TransitionHandler;
  * Created by Kai on 2015/8/6.
  */
 public abstract class ScaledTransitionHandler implements TransitionHandler {
+    private float prevProgress = Float.MIN_VALUE;
+    private boolean minProgressUpdated;
+    private boolean maxProgressUpdated;
 
     @Override
     public final void onUpdateProgress(TransitionController controller, View target, float progress) {
-        float start = controller.getStart();
-        float end = controller.getEnd();
-        if (progress < start || progress > end) {
-            return;
+        int direction = 0; //1 == increasing, -1 == decreasing
+        if (prevProgress != Float.MIN_VALUE) {
+            direction = progress > prevProgress ? 1 : -1;
         }
-        float duration = end - start;
-        onUpdateScaledProgress(controller, target, (progress - start) / duration);
+
+        if (direction == -1) {  //decreasing
+            if (progress < 0) {
+                if (minProgressUpdated) {
+                    return;
+                }
+                minProgressUpdated = true;
+                progress = 0;
+            }
+            maxProgressUpdated = false;
+        } else if (direction == 1) { //increasing
+            if (progress > 1) {
+                if (maxProgressUpdated) {
+                    return;
+                }
+                maxProgressUpdated = true;
+                progress = 1;
+            }
+            minProgressUpdated = false;
+        }
+        onUpdateScaledProgress(controller, target, progress);
     }
 
     protected abstract void onUpdateScaledProgress(TransitionController controller, View target, float modifiedProgress);
